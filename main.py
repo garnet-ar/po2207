9349f890e78beddea4403578b72b4e0d2e5ceb90
Aruzhan Anarkulova <122878535+garnet-ar@users.noreply.github.com>
2023-12-25 09:22:21 +0600

    v0.2
    Добавила функцию для подключения к онлайн бд

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('My second project')
+from _pydecimal import Decimal
+
+import psycopg2
+
+try:
+    conn = psycopg2.connect(host = '78.141.227.124',
+                            user = 'postgres',
+                            database = 'GITHUB',
+                            password = '123')
+    print('Успешно подключено к Базе Данных')
+
+    cur = conn.cursor()
+    # cur.execute("select count(age) from students where age>15") 1-задание
+    # cur.execute("select round(avg(age),1) from students where stud_name ILIKE 'A%'") 2-задание
+    avg = cur.fetchall()
+    print(avg)
+except Exception as err:
+    print(err)
